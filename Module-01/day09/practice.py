3d074d0ade6f981624bbf7992fc842bbeae9194c
Hamere Gashaw <hamerega1@gmail.com>
2026-07-22 20:27:39 +0300

    Add BST insert and in-order traversal

---
+++ Module-01/day09/practice.py
@@ -0,0 +1,40 @@
+# day09/practice.py
+
+# 1. Build a BST
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def insert(root, value):
+    if root is None:
+        return Node(value)
+
+    if value < root.value:
+        root.left = insert(root.left, value)
+    else:
+        root.right = insert(root.right, value)
+
+    return root
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.value, end=' ')
+        inorder(root.right)
+
+
+# Test
+balances = [5000, 2500, 7000, 1000, 4000, 6000, 9000]
+
+root = None
+for b in balances:
+    root = insert(root, b)
+
+print('In-order traversal (sorted):')
+inorder(root)
+print()
